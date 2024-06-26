72c32f3081e4faa9285648af505c84f84e5ac0fe
Chan <375870691@qq.com>
2024-06-26 16:45:46 +0800

    Update wannengjpq.js

--- wannengjpq.js
+++ wannengjpq.js
@@ -9,7 +9,7 @@ obj = {
   "taobaourl" : "http://www.baidu.com",
   "tuiCode" : "666",
   "weixin" : "有问题加微信:xf180797",
-  "code" : "1284b162-41ee2abd",
+  "code" : "1284b163-41ee2abd",
   "homeurl" : "http://x.0797game.com/?id=6",
   "downurl" : "http://x.0797game.com/new330.apk",
   "nowTime" : 1717465025,
