5241ed97a137b0707fe9425b697fcacd1c248b7f
Chan <375870691@qq.com>
2024-07-08 10:52:15 +0800

    Update wannengjpq.js

--- wannengjpq.js
+++ wannengjpq.js
@@ -3,16 +3,16 @@ var obj = JSON.parse(body);
 
 obj = {
   "priceStr" : "18;88;263",
-  "maxver" : "220",
+  "maxver" : "230",
   "notice" : "感谢您使用万能记牌器，有任何问题请咨询微信客服。仅供娱乐，禁止赌博。",
-  "minver" : "264",
+  "minver" : "254",
   "taobaourl" : "http://www.baidu.com",
   "tuiCode" : "666",
   "weixin" : "有问题加微信:xf180797",
-  "code" : "",
+  "code" : "75c5c1bf-d5d8c1c6",
   "homeurl" : "http://x.0797game.com/?id=6",
   "downurl" : "http://x.0797game.com/new330.apk",
-  "nowTime" : 1717465025,
+  "nowTime" : 1720406750020,
   "dian" : 100000,
     "huasedata" : [
     "bbabf819532a45f7e25052be055472b0bcd7781",
