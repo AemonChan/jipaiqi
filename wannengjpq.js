c879cf6c257b1c68a38348d798826c9bb8ee1f6c
Chan <375870691@qq.com>
2024-07-23 14:47:08 +0800

    Update wannengjpq.js

--- wannengjpq.js
+++ wannengjpq.js
@@ -13,7 +13,7 @@ obj = {
   "homeurl" : "http://x.0797game.com/?id=6",
   "downurl" : "http://x.0797game.com/new330.apk",
   "nowTime" : 1719210851000,
-  "dian" : 100000,
+  "dian" : 100,
     "huasedata" : [
     "bbabf819532a45f7e25052be055472b0bcd7781",
     "bbbb5f0364da3ef0522b5d0ef653c3cb9325989",
